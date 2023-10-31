54ba536fb8e47c1ec0f9e17824c225cc6c096c89
Gayratbek <gayratbekakhmedov@gmail.com>
2023-10-31 12:09:12 +0500

    first commit

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:RcitqEqL6fXb@109.94.172.130:5432/mastika"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:RcitqEqL6fXb@localhost:5432/mastika"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
