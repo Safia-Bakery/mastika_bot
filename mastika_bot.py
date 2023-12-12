429f7ad6153ce59257cef848f67b80694aa2ea66
Gayratbek <gayratbekakhmedov@gmail.com>
2023-12-12 09:54:11 +0500

    sdf

--- mastika_bot.py
+++ mastika_bot.py
@@ -216,7 +216,7 @@ def main() -> None:
             VARIFICATION:[MessageHandler(filters.TEXT,varification)],
             FULLNAME:[MessageHandler(filters.TEXT,fullname)],
             MANU:[MessageHandler(filters.TEXT,manu)],
-            ORDER:[MessageHandler(filters.TEXT,order)],
+            ORDER:[MessageHandler(filters.StatusUpdate.WEB_APP_DATA,order)],
             INFORMATION:[MessageHandler(filters.TEXT,information)],
             SETTINGS:[MessageHandler(filters.TEXT,settings)],
             UPDATEFULLNAME:[MessageHandler(filters.TEXT,updatefullname)],
